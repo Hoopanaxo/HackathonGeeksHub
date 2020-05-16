40c97579385dc0d36a5768c056675ae168b75857
Pablo Casas <casdelprog@gmail.com>
2020-05-16 13:31:58 +0200

    Bot con 3 handlers

--- Kata3/geeksbot.py
+++ Kata3/geeksbot.py
@@ -1,3 +1,7 @@
+"""
+
+Primer bot de prueba, es necesario crear el ficher bot_token
+"""
 from telegram.ext import Updater, CommandHandler
 
 
@@ -5,9 +9,13 @@ def main():
     #añadir token pedido a BotFather ( en telegram )
     updater = Updater(token=open("./bot_token").read(), use_context=True)
     
-    #añade el manejador para el comando start
+    #añade el manejador para el comando /start
     updater.dispatcher.add_handler(CommandHandler("start", start))
+    
+    #añade manejador para el comando /repite
+    updater.dispatcher.add_handler(CommandHandler("repite", repite))
 
+    updater.dispatcher.add_handler(CommandHandler("suma", suma))
     #Empezamos a pedir notificaciones
     updater.start_polling()
 
@@ -16,6 +24,12 @@ def main():
 
 def start(update, context):
     update.message.reply_text("Hello world")
-    pass
+    
+def repite(update, context):
+    update.message.reply_text(update.message.text)
+
+def suma(update, context):
+    suma = int(context.args[0]) + int(context.args[1])
+    update.message.reply_text(f"El resultado es: {suma}")
 
 main()
